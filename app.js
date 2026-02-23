8efc149bfb4e2bf1e018c4798519771d02eb270f
igcurrais <nacho.currais@gmail.com>
2026-02-22 22:46:30 -0300

    Version inicial con desplegable

---
+++ app.js
@@ -0,0 +1,74 @@
+//const geoURL = "https://geocoding-api.open-meteo.com/v1/search?name=Buenos Aires&count=1"
+const boton = document.getElementById("boton");
+const resultado = document.getElementById("resultado");
+const ciudad = document.getElementById("ciudad");
+
+const lugares = {
+    sanMartin: {name: "General San Martin",latitud:-34.57789,longitud:-58.53864},
+    ballester: {name: "Villa Ballester",latitud:-34.54531,longitud:-58.55919},
+    belgrano: {name: "Belgrano",latitud:-34.5627,longitud:-58.45829},
+    villaLuro: {name: "Villa Luro",latitud:-34.63814,longitud:-58.50273},
+    marDelPlata: {name:"Mar Del Plata",latitud:-38.00042,longitud:-57.5562},
+    ushuaia: {name:"Ushuaia",latitud:-54.81084,longitud:-68.31591},
+
+}
+
+const estados ={
+   0: "Despejado ☀️",
+   1: "Mayormente despejado 🌤️",
+   2: "Parcialmente nublado ⛅",
+   3: "Nublado ☁️",
+   45: "Niebla 🌫️",
+   61: "Lluvia 🌧️",
+   95: "Tormenta ⛈️"
+}
+/*
+async function geoLocalizacion() {
+    let data;
+    try {
+        const res = await fetch(geoURL);
+        data = await res.json();
+        console.log(data);
+    }catch (error){
+        console.log(error);
+    }
+    if (data.results && data.results.length > 0) {
+        return{
+            name: data.results[0].name,
+            latitud: data.results[0].latitude,
+            longitud: data.results[0].longitude
+        };        
+    } else {
+        return null;
+    }
+    
+}*/
+
+async function obtenerClima(lugarselect) {
+    //const ubicacion = await geoLocalizacion();
+    //if(!ubicacion)return;
+    
+
+    try{
+        const climaURL = `https://api.open-meteo.com/v1/forecast?latitude=${lugares[lugarselect].latitud}&longitude=${lugares[lugarselect].longitud}&current_weather=true`;
+        const res = await fetch(climaURL);
+        const data = await res.json();
+
+        console.log(data);
+        const tempActual = data.current_weather.temperature;
+        const cielo = data.current_weather.weathercode;
+        resultado.innerHTML = "";
+        const texto = document.createElement("h3");
+        texto.textContent = `Temperatura actual en ${lugares[lugarselect].name} es de ${tempActual} con el cielo ${estados[cielo] || "Desconocido"}`;
+        
+        resultado.appendChild(texto);
+
+    }catch(error){
+        console.log(error);
+    }    
+}
+
+boton.addEventListener("click", ()=>{
+    let lugarselect = ciudad.value;
+    obtenerClima(lugarselect);
+});
